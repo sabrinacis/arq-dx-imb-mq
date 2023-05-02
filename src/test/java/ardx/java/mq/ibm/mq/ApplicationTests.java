a4e28c380f8605d663b499d164571210679879ab
Cis <sabrina.cis@accenture.com>
2023-05-02 14:45:17 -0300

    inicio lib

---
+++ src/test/java/ardx/java/mq/ibm/mq/ApplicationTests.java
@@ -0,0 +1,13 @@
+package ardx.java.mq.ibm.mq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
